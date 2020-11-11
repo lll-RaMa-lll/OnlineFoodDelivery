2bed0e756db3806541b750c3ec5633714f4a5432
RaMa <1.rajarshi.mondal@gmail.com>
2020-11-11 20:04:08 +0530

    added more auth functions

--- controllers/auth.js
+++ controllers/auth.js
@@ -119,3 +119,27 @@ exports.signin = (req, res) => {
         });
     });
 };
+
+exports.isSignedIn = expressJwt({
+    secret: process.env.SECRET,
+    userProperty: "auth"
+});
+
+exports.isAuthenticated = (req, res, next) => {
+    let checker = req.profile && req.auth && req.profile._id == req.auth._id;
+    if (!checker) {
+        return res.status(403).json({
+            error: "Acccess Denied"
+        });
+    }
+    next();
+}
+
+exports.isAdmin = (req, res, next) => {
+    if (!req.profile.admin) {
+        return res.status(403).json({
+            error: "Admin access not granted"
+        })
+    }
+    next();
+}

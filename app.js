93c0ce7cdb137a2c8e631ee2ae38b2fea9bf4cda
EliteLabyrinth <soumabratabhattacharya@gmail.com>
2020-12-01 17:58:42 +0530

    all the socket logics are added

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const mongoose = require("mongoose");
+const http = require('http')
 const express = require("express");
+const socketio = require('socket.io')
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
@@ -11,6 +13,11 @@ const restaurantRoute = require("./routes/restaurant");
 const customerRoute = require("./routes/user_customer");
 const valetRoute = require("./routes/user_valet");
 const foodRoutes = require("./routes/food");
+
+
+
+const { addUser, removeUser, getUser ,getAllUsers} = require('./socket_connections')
+
 
 //DB connection
 mongoose.connect(process.env.DATABASE, {
@@ -25,6 +32,8 @@ mongoose.connect(process.env.DATABASE, {
     });
 
 const app = express();
+const server = http.createServer(app)
+const io = socketio(server)
 
 //middlewares
 app.use(bodyParser.json());
@@ -38,22 +47,136 @@ app.use("/api", customerRoute);
 app.use("/api", valetRoute);
 app.use("/api",foodRoutes);
 
-app.get("/",(req,res)=>{
-    res.redirect()
-})
 
-app.get("/sse-server",(req,res,next)=>{
-    res.status(200).set({
-        "connection": "keep-alive",
-        "cache-control": "no-cache",
-        "Content-Type" : "text/event-stream"
+
+io.on('connect',(socket)=>{
+
+    console.log('connected')
+    socket.on('customerConnection',(msg,callback)=>{
+        console.log(msg)
+        addUser(msg,socket,'customer')
+        callback('user successfully connected')
     })
 
-    res.write(`data: Hello world!\n\n`)
+    socket.on('restaurantConnection',(msg,callback)=>{
+        console.log(msg)
+        addUser(msg,socket,'restaurant')
+        callback('restaurant successfully connected')
+    })
+
+    socket.on('valetConnection',(msg,callback)=>{
+        console.log(msg)
+        addUser(msg,socket,'valet')
+        socket.join('valet')
+        callback('valet successfully connected')
+    })
+
+    socket.on('orderPlaced',(data)=>{
+        console.log(data)
+        socket.emit('responseToUser',{message:'order is saved.waiting for restaurant update'})
+
+        const {userName,restaurantName} = data
+        const restaurant = getUser(restaurantName,'restaurant')
+        
+        if(!restaurant) {socket.emit('errorMessage',{error:'restaurant is not online right now'})}
+        else{
+            socketRestaurant=restaurant.socket
+
+            socketRestaurant.emit('orderForRestaurant',data)
+
+            socketRestaurant.on('responseToServerRegardingOrderFromRestaurant', (data)=>{
+                console.log(data)
+                if(data.isAcceptingOrder){
+        
+                    socket.emit('responseToUser',{message:'restaurant has accepted the order'})
+        
+                    // io.to('valet').emit('orderForVallet',data)
+                    // console.log('list of clients')
+                    // console.log(io.sockets.clients().sockets)
+                    let valetList=getAllUsers('valet')
+
+                    for(let valet of valetList){
+                        valetSocket=valet.socket
+                        valetSocket.emit('orderForValet',{name:valet.name,isAcceptingOrder:data.isAcceptingOrder})
+                    }
+
+                    let valetAcceptingOrder = ''
+                    let counter=0
+
+                    // console.log(valetList)
+                    for(let valet of valetList){
+
+                        valetSocket=valet.socket
+                        valetSocket.on('responseToServerRegardingOrderFromValet',(data)=>{
+                            counter+=1
+                            console.log(data)
+                            if(data.hasAcceptedOrder){
+                                if(!valetAcceptingOrder){ 
+                                    
+                                    valetAcceptingOrder=valet
+                                    socket.emit('responseToUser',{message:`valet named ${data.name} has accepted the order`})
+                                    socketRestaurant.emit('responseToRestaurantRegardingValetInformation',{msg:`valet named ${data.name} has accepted the order`})
+
+                                    for (let newValet of valetList){
+                                        if(newValet!==valet){
+                                            newValet.socket.emit('messageToValetsRegardingAcceptanceOfOrder',{msg:`valet named ${data.name} has accepted the order`})
+                                        }
+    
+                                    }
+
+                                    valetAcceptingOrder.socket.emit('messageToValetsRegardingAcceptanceOfOrder',{msg:`user is notified!`})
+                                
+                                }
+
+                                
+                            }else{
+                                if(counter===valetList.length && !valetAcceptingOrder){
+                                    socket.emit('responseToUser',{message:'no valets available'})
+                                }
+                            }
+
+                        })
+                        if(valetAcceptingOrder){
+                            break
+                        }
+                    }
+                
+
+
+                }else{
+                    socket.emit('responseToUser',{message:'restaurant has rejected the order'})
+                }   
+            })
+        }
+    })
+
+
+                 
+
+
+
+    socket.on('disconnect',()=>{
+        const user = removeUser(socket.id)
+        if(user) console.log(`${user.name} left`)
+    })
+
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //Starting server
 const port = process.env.SERVERPORT;
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`app is running on port ${port}`);
 });

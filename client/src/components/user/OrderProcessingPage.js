f4f66fe0df26708c28f7631bf76339b6654544b9
RaMa <1.rajarshi.mondal@gmail.com>
2020-12-30 23:12:46 +0530

    added style to Order Processing page

--- client/src/components/user/OrderProcessingPage.js
+++ client/src/components/user/OrderProcessingPage.js
@@ -1,35 +1,54 @@
-import React,{useState,useEffect} from 'react'
-import {socket} from '../../socket'
-import {Button} from '@material-ui/core'
-import {loadCart,cartEmpty } from '../common/helper/carthelper'
+import React, { useState, useEffect } from 'react'
+import { socket } from '../../socket'
+import { Button } from '@material-ui/core'
+import { loadCart, cartEmpty } from '../common/helper/carthelper'
+import UserBase from './UserBase'
 
-export default function OrderProcessingPage(){
+export default function OrderProcessingPage() {
 
-    const [message,setMessage] = useState('Order Processing...')
+    const [message, setMessage] = useState('Order Processing...')
     // const [error,setError] = useState('')
-    useEffect(()=>{
+    useEffect(() => {
 
         let orderData = loadCart()
-        socket.emit('orderPlaced',{orderData})
+        socket.emit('orderPlaced', { orderData })
 
-        socket.on('responseToUser',(data)=>{
+        socket.on('responseToUser', (data) => {
             cartEmpty()
             console.log(data)
             setMessage(data.message)
         })
 
-        socket.on('errorMessage',(err)=>{
+        socket.on('errorMessage', (err) => {
             console.log(err)
             // setError(err)
         })
 
-    },[])
-    
+    }, [])
+
     return (
-        <div>
-            <h1>{message}</h1>
-            {/* <h1>{error}</h1> */}
-        </div>
+        <UserBase isSignedIn={true}>
+            {message == 'Order sent to restaurant' &&
+                <div>
+                    <h1>{message}</h1>
+                </div>
+            }
+            {message == 'Order accepted by restaurant' &&
+                <div>
+
+                </div>
+            }
+            {message == 'Valet assigned' &&
+                <div>
+
+                </div>
+            }
+            {message == 'Order placed' &&
+                <div>
+
+                </div>
+            }
+        </UserBase>
     )
 
 

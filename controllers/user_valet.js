fb4b9cc547b591d68a63e05aac6c34795072065c
EliteLabyrinth <soumabratabhattacharya@gmail.com>
2020-12-31 07:11:15 +0530

    update valet is modified

--- controllers/user_valet.js
+++ controllers/user_valet.js
@@ -1,4 +1,5 @@
 const User_valet = require("../models/user_valet");
+const _ = require("lodash");
 
 
 exports.getUserById = (req, res, next, id) => {
@@ -20,9 +21,35 @@ exports.getUser = (req, res) => {
 };
 
 exports.updateUser = (req, res) => {
+  let {name,email,phone,details} = req.body
+  let user = {}
+  if(name){
+    user={...user,name}
+  }
+  if(email){
+    user={...user,email}
+  }
+  if(phone){
+    user={...user,phone}
+  }
+
+  let valet = req.profile
+  valet = _.extend(valet,user)
+  console.log(valet)
+  
+  
+  // user.save((err, user) => {
+  //   if (err) {
+  //     res.status(400).json({
+  //       error: "Updation of Customer data failed"
+  //     });
+  //   }
+  //   res.json(user);
+  // });
+
   User_valet.findByIdAndUpdate(
     { _id: req.profile._id },
-    { $set: req.body },
+    { $set: valet },
     { new: true, useFindAndModify: false },
     (err, user) => {
       if (err) {
@@ -36,4 +63,3 @@ exports.updateUser = (req, res) => {
     }
   );
 };
-

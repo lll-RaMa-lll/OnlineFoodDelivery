d11d8011219ed225e4a009398beccbfa8796d915
RaMa <1.rajarshi.mondal@gmail.com>
2020-12-31 07:28:15 +0530

    added styling

--- client/src/components/user/OrderProcessingPage.js
+++ client/src/components/user/OrderProcessingPage.js
@@ -1,12 +1,35 @@
 import React, { useState, useEffect } from 'react'
 import { socket } from '../../socket'
-import { Button } from '@material-ui/core'
+import { Button, createMuiTheme, makeStyles } from '@material-ui/core'
 import { loadCart, cartEmpty } from '../common/helper/carthelper'
 import UserBase from './UserBase'
 import Animation from '../common/animation'
+import { CheckCircle, CheckCircleOutline } from '@material-ui/icons'
 
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: "#FF5544",
+        },
+        secondary: {
+            main: "#FFFF88",
+        },
+    },
+});
+const useStyles = makeStyles((theme) => ({
+    texts: {
+        color: '#FFF',
+    },
+    check: {
+        position: 'relative',
+        margin: '0.2em 0.2em 0.2em 1.2em',
+        top: '0.5em',
+        fontSize: '2em',
+        color: '#448844'
+    },
+}));
 export default function OrderProcessingPage() {
-
+    const classes = useStyles();
     const [message, setMessage] = useState('Processing Order')
     // const [error,setError] = useState('')
     useEffect(() => {
@@ -30,23 +53,33 @@ export default function OrderProcessingPage() {
     return (
         <UserBase isSignedIn={true}>
             {message == 'Processing Order' &&
-                <div>
-                    <h1>{message}</h1>
+                <div className={classes.texts}>
+                    <CheckCircle className={classes.check} />Processing Order
+                    <CheckCircleOutline className={classes.check} />Order sent to restaurant
                 </div>
             }
             {message == 'order is saved.waiting for restaurant update' &&
-                <div>
-                    <h1>Order sent to restaurant</h1>
+                <div className={classes.texts}>
+                    <CheckCircle className={classes.check} />Processing Order<br />
+                    <CheckCircle className={classes.check} />Order sent to restaurant<br />
+                    <CheckCircleOutline className={classes.check} />Restaurant has accepted the Order<br />
                 </div>
             }
             {message == 'restaurant has accepted the order' &&
-                <div>
-                    <h1>Restaurant has accepted the Order</h1>
+                <div className={classes.texts}>
+                    <CheckCircle className={classes.check} />Processing Order<br />
+                    <CheckCircle className={classes.check} />Order sent to restaurant<br />
+                    <CheckCircle className={classes.check} />Restaurant has accepted the Order<br />
+                    <CheckCircleOutline className={classes.check} />Valet assigned
+
                 </div>
             }
-            {message.match(/valet named [A-Za-z]* has accepted the order/) != undefined &&
-                <div>
-                    <h1>Valet assigned</h1>
+            {message.match(/valet named/) != undefined &&
+                <div className={classes.texts}>
+                    <CheckCircle className={classes.check} />Processing Order<br />
+                    <CheckCircle className={classes.check} />Order sent to restaurant<br />
+                    <CheckCircle className={classes.check} />Restaurant has accepted the Order<br />
+                    <CheckCircle className={classes.check} />Valet assigned
                 </div>
             }
             <Animation></Animation>

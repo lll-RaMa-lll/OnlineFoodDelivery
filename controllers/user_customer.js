4fd26eb2d154768b95f34ae53fe20d7279cba603
EliteLabyrinth <soumabratabhattacharya@gmail.com>
2020-12-31 07:11:02 +0530

    Update user is modified

--- controllers/user_customer.js
+++ controllers/user_customer.js
@@ -1,4 +1,6 @@
 const User_customer = require("../models/user_customer");
+const _ = require("lodash");
+
 
 
 exports.getUserById = (req, res, next, id) => {
@@ -20,9 +22,38 @@ exports.getUser = (req, res) => {
 };
 
 exports.updateUser = (req, res) => {
+  let {name,email,phone,address} = req.body
+  let user = {}
+  if(name){
+    user={...user,name}
+  }
+  if(email){
+    user={...user,email}
+  }
+  if(phone){
+    user={...user,phone}
+  }
+  if(address){
+    user={...user,address}
+  }
+
+  let customer = req.profile
+  customer = _.extend(customer,user)
+  console.log(customer)
+  
+  
+  // user.save((err, user) => {
+  //   if (err) {
+  //     res.status(400).json({
+  //       error: "Updation of Customer data failed"
+  //     });
+  //   }
+  //   res.json(user);
+  // });
+
   User_customer.findByIdAndUpdate(
     { _id: req.profile._id },
-    { $set: req.body },
+    { $set: customer },
     { new: true, useFindAndModify: false },
     (err, user) => {
       if (err) {
